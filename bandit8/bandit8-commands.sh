299a7d807d67296d3b64a592ec06bbc4b4ef8879
Bruce Marlier <86775384+brucemarlier@users.noreply.github.com>
2025-10-19 20:14:14 +0200

    Bandit8

--- bandit8/bandit8-commands.sh
+++ bandit8/bandit8-commands.sh
@@ -6,7 +6,7 @@ cat data.txt | grep "millionth"
 # millionth	dfwvzFQi4mU0wfNbFOe9RoWskMLg7eEc
 
 # Alternate command line to remove the word "millionth" and blank spaces to isolate the password
-cat data.txt | grep "millionth" | tr -d "millionth [:space:]"
+cat data.txt | grep "millionth" | sed 's/\bmillionth\b//g' | tr -d "[:blank:]"
 
 # Output:
 # dfwvzFQi4mU0wfNbFOe9RoWskMLg7eEc
