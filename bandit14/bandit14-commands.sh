ec8ef74575cc29d5fa25d987493c20fd9afd27e1
Bruce Marlier <86775384+brucemarlier@users.noreply.github.com>
2025-10-20 01:54:43 +0200

    Bandit14

---
+++ bandit14/bandit14-commands.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+ls
+
+# Output:
+# sshkey.private
+
+cat sshkey.private
+
+# Output:
+# private key details
+
+exit
+
+# Output:
+# logout
+# Connection to bandit.labs.overthewire.org closed.
+
+# On my own computer's terminal
+# Create a file with key I copied from bandit13's home directory on server
+echo "private key details here" >> sshkey2.private
+
+# Make file executable
+chmod 700 sshkey.private
+
+# Connect to next level using the key identity
+ssh -i sshkey.private bandit14@bandit.labs.overthewire.org -p 2220
