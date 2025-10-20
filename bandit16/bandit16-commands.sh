c7d231dab921714d5ce8f59a8b8592577079af10
Bruce Marlier <86775384+brucemarlier@users.noreply.github.com>
2025-10-20 03:01:17 +0200

    Bandit16

---
+++ bandit16/bandit16-commands.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+openssl s_client localhost:30001
+
+# Output:
+# a lot of data about the server is shown, asks for user input
+
+# Input password of current level
+8xCjnmgoKbGLhHFAZlGE5Tmu4M2tKJQo
+
+# Output:
+# Correct!
+# kSkvUpMQ7lBYyCM4GBPvCvT1BfWRy0Dx
